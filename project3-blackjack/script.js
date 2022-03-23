bd7ec234f33a043cb276fa107968a9e9e11a105d
Bryan Luke Tan <69680070+Nuuggs@users.noreply.github.com>
2022-03-23 15:54:09 +0800

    submission

--- project3-blackjack/script.js
+++ project3-blackjack/script.js
@@ -1,4 +1,20 @@
+// 2 kinds of SCOPE - GLOBAL && FUNCTIONAL
+// Global is outside everything else
+console.log("this is in global scope");
+var MODE_START = "start";
+
+var MODE = MODE_START;
+
+var USERNAME = "";
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  console.log("checking current mode: " + MODE);
+  if (MODE == MODE_START) {
+    if (input != "") {
+      USERNAME = input;
+      MODE = DRAW_CARD;
+      return "Hello " + input + "Welcome to my Blackjack game!";
+    }
+    return "Please enter your name!";
+  }
 };
